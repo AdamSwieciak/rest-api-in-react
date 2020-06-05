0d81de2d80b6d621268dc5d50399b1da0d9b8f89
AdamSwieciak <adamswiec@gmail.com>
2020-06-05 15:59:15 +0200

    Updates

---
+++ precache-manifest.2a59fe23a8430804d27345761832acb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd77d0e4d316497e5288815a12e68ebb",
+    "url": "/rest-api-in-react/index.html"
+  },
+  {
+    "revision": "2953ef253c37660e7100",
+    "url": "/rest-api-in-react/static/css/main.0e80d763.chunk.css"
+  },
+  {
+    "revision": "1816ead106b7bb3e59dd",
+    "url": "/rest-api-in-react/static/js/2.82b13777.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rest-api-in-react/static/js/2.82b13777.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2953ef253c37660e7100",
+    "url": "/rest-api-in-react/static/js/main.191d92dd.chunk.js"
+  },
+  {
+    "revision": "0518e7c3cf1bfab09396",
+    "url": "/rest-api-in-react/static/js/runtime-main.90257abb.js"
+  }
+]);
